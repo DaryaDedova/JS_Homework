e9a5719724d517e28d86f546994098ef53e0f372
Darya Dedova <dasha_260899@mail.ru>
2023-10-11 19:54:16 +0300

    Добавлено ДЗ с инпутами в банковской карте

--- ДЗ#2 jQ/script.js
+++ ДЗ#2 jQ/script.js
@@ -36,6 +36,55 @@ $(document).ready(function () {
 
 
 
+    
+    $(function(){
+        $('.buttE').click(function(e){
+            e.preventDefault();
+            if($('.cardNumber').val() != '' && $('.cardName').val() != '' && $('.dayCard').val() != '' && $('.yearCard').val() != '' && $('.pinCard').val() != ''){
+                $('.modal').hide();
+            } else{
+                if($('.cardNumber').val() != ''){
+                    $('.cardNumber').attr('disabled', 'disabled');
+                }
+                else {
+                    alert('Поле Card Number не заполнено')
+                }
+                if($('.cardName').val() != ''){
+                    $('.cardName').attr('disabled', 'disabled');
+                }
+                else {
+                    alert('Поле Name On Card не заполнено')
+                }
+                if($('.dayCard').val() != ''){
+                    $('.dayCard').attr('disabled', 'disabled');
+                }
+                else {
+                    alert('Поле Date не заполнено')
+                }
+                if($('.yearCard').val() != ''){
+                    $('.yearCard').attr('disabled', 'disabled');
+                }
+                else {
+                    alert('Поле год не заполнено')
+                }
+                if($('.pinCard').val() != ''){
+                    $('.pinCard').attr('disabled', 'disabled');
+                }
+                else {
+                    alert('Поле CVV не заполнено')
+                }
+            }
+            
+        })
+    })
+
+
+
+
+    $('.cardNumber').mask("9999 9999 9999 9999", {autoclear: false});
+    $('.dayCard').mask('99');
+    $('.yearCard').mask('9999');
+
 
     // $('.small a img').click(function(){
     //     $(this).fadeTo(600, 0.6).css(
